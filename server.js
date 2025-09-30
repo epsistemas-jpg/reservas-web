db136f6a19cc2e57e7c5762de15123d7565932f0
epsistemas-jpg <epsistemas@e2energiaeficiante.com>
2025-09-30 12:23:08 -0500

    Actualizacion 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { Pool } = require("pg");
 const path = require("path");
 
